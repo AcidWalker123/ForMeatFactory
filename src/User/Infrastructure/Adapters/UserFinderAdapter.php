ce09354359bbcff39b2cc84ad4e687e7d84c8e35
AcidWalker123 <samushao321@gmail.com>
2025-07-23 19:13:11 +0400

    Update UserFinderAdapter.php

--- src/User/Infrastructure/Adapters/UserFinderAdapter.php
+++ src/User/Infrastructure/Adapters/UserFinderAdapter.php
@@ -1,9 +1,11 @@
 <?php
 
 namespace App\User\Infrastructure\Adapters;
+
 use App\Order\Application\Port\UserFinderInterface;
 use App\User\Infrastructure\Doctrine\Persistence\UserRepository;
 use App\User\Domain\Entity\User;
+
 class UserFinderAdapter implements UserFinderInterface
 {
     private UserRepository $userRepository;
